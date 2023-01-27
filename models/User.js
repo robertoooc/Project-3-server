6073edd5760e2263d9e7362c0b09278232169f79
YOUR-USERNAME <gtellez07@yahoo.com>
2023-01-27 16:22:30 -0600

    updated schema, added bio

--- models/User.js
+++ models/User.js
@@ -3,13 +3,17 @@ const mongoose = require('mongoose')
 
 const UserSchema = new mongoose.Schema({
   name: {
-    type: String
+    type: String,
+    required: true
   },
   email: {
-    type: String
+    type: String,
+    required: true,
+    unique: true
   },
   password: {
-    type: String
+    type: String,
+    required: true
   }
 }, {
   timestamps: true

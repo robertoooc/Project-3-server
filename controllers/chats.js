dc068662b6276a8f92cf7bcc897dea92b9d82430
robertoooc <robertooo.c48@gmail.com>
2023-01-29 12:31:17 -0800

    included some basic error handling for chat routes

--- controllers/chats.js
+++ controllers/chats.js
@@ -8,16 +8,18 @@ router.get('/', async function (req,res){
         res.json({msg: 'Welcome to chats 💬 ', chats})
     }catch(err){
         console.log(err)
-        res.status(500).json({msg: 'server error'})
+        res.status(500).json({msg: 'my bad G'})
     }
 })
 
 router.get('/:id', async function (req,res){
     try{
         const findChatRoom = await db.Chat.findById(req.params.id)
+        if(!findChatRoom) return res.status(404).json({ msg: "You messed up" });
         res.json(findChatRoom)
     }catch(err){
         console.log(err)
+        res.status(500).json({msg: 'my bad G'})
     }
 })
 
@@ -26,28 +28,34 @@ router.post('/', async function (req,res){
         const newChatRoom = await db.Chat.create(req.body)
         res.json(newChatRoom)
     }catch(err){
-        console.log(err)
+        console.log(err._message)
+        if (err._message == 'Chat validation failed') return res.status(400).json({msg:"You messed up, User's fault"})
+        res.status(500).json({msg: 'my bad G'})
     }
 })
 
 router.get('/:id/comment', async function (req,res){
     try{
         const findChatRoom = await db.Chat.findById(req.params.id)
+        if(!findChatRoom) return res.status(404).json({ msg: 'You messed up' });
         res.json(findChatRoom)
     }catch(err){
         console.log(err)
+        res.status(500).json({msg: 'my bad G'})
     }
 })
 
 router.post('/:id/comment', async function (req,res){
     try{
         const findChatRoom = await db.Chat.findById(req.params.id)
+        if(!findChatRoom) return res.status(404).json({ msg: 'You messed up' });
         const addComment = req.body
         findChatRoom.content.push(addComment)
         await findChatRoom.save()
         res.json(findChatRoom)
     }catch(err){
-        console.log(err)
+        console.log(err.kind)
+        res.status(500).json({msg: 'my bad G'})
     }
 })
 
